118b96487ef787f396f59ea493899c61fa23d438
Berend de Boer <berend@pobox.com>
2026-05-07 13:21:45 +1200

    Avoid temp file for preserved pacman repo

--- install/helpers/pacman.sh
+++ install/helpers/pacman.sh
@@ -1,25 +1,21 @@
 use_omarchy_pacman_config() {
   local mirror="${OMARCHY_MIRROR:-stable}"
-  local archzfs_repo
-
-  archzfs_repo=$(mktemp)
+  local archzfs_repo=""
 
   if [[ -f /etc/pacman.conf ]]; then
-    awk '
+    archzfs_repo=$(awk '
       /^\[/ { in_archzfs = ($0 == "[archzfs]") }
       in_archzfs { print }
-    ' /etc/pacman.conf >"$archzfs_repo"
+    ' /etc/pacman.conf)
   fi
 
   sudo cp -f "$OMARCHY_PATH/default/pacman/pacman-$mirror.conf" /etc/pacman.conf
   sudo cp -f "$OMARCHY_PATH/default/pacman/mirrorlist-$mirror" /etc/pacman.d/mirrorlist
 
-  if [[ -s $archzfs_repo ]]; then
+  if [[ -n $archzfs_repo ]]; then
     printf "\n" | sudo tee -a /etc/pacman.conf >/dev/null
-    sudo tee -a /etc/pacman.conf <"$archzfs_repo" >/dev/null
+    printf "%s\n" "$archzfs_repo" | sudo tee -a /etc/pacman.conf >/dev/null
   fi
-
-  rm -f "$archzfs_repo"
 }
 
 export -f use_omarchy_pacman_config
